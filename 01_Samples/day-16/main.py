ea1f7e5019a4694a67c7d837132ed1fdcb5897aa
SkandanRIyer <rajeshtechier@gmail.com>
2022-02-06 13:36:15 +0530

    Day16 commit

---
+++ 01_Samples/day-16/main.py
@@ -0,0 +1,16 @@
+# from turtle import Turtle, Screen
+
+# ranga = Turtle()
+# ranga.penup()
+# ranga.color("green")
+# ranga.shape("turtle")
+# ranga.forward(100)
+# screen = Screen()
+# screen.exitonclick()
+#
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name",["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
