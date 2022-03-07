dbb4c1ce8fd50ab7b41ec34b645f773ca1e2af1c
SkandanRIyer <rajeshtechier@gmail.com>
2022-03-07 19:52:36 +0530

    Day 31 code

--- 01_Samples/17-Flash-card/main.py
+++ 01_Samples/17-Flash-card/main.py
@@ -41,6 +41,7 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+
 canvas = Canvas(width=800, height=526, highlightthickness=0)
 front_photo = PhotoImage(file="./images/card_front.png")
 back_photo = PhotoImage(file="./images/card_back.png")
